6418752b674dfe4556c15ff034309e0c7ee0d527
rakesh635 <rakesh635@gmail.com>
2022-07-05 19:03:04 +0530

    Update AddressbookUI.java

--- src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
@@ -38,7 +38,7 @@ public class AddressbookUI extends UI {
      */
     TextField filter = new TextField();
     Grid contactList = new Grid();
-    Button newContact = new Button("Add New Contact...");
+    Button newContact = new Button("Add New Contact4321");
 
     // ContactForm is an example of a custom component class
     ContactForm contactForm = new ContactForm();
